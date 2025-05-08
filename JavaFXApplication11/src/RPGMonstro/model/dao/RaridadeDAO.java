9d769011d852d29cdfb8af150737f84a4dc14d24
mugdots <danielssauro1000@hotmail.com>
2025-05-08 16:14:41 -0300

    AAAA

---
+++ JavaFXApplication11/src/RPGMonstro/model/dao/RaridadeDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RPGMonstro.model.dao;
+
+/**
+ *
+ * @author 20231si008
+ */
+public class RaridadeDAO {
+    
+}
